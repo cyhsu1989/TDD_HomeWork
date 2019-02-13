245f71a70f49584993d0c930f86dd5cab92a26f3
KyleHsu <kylehsu@reallusion.com>
2019-02-13 19:28:55 +0800

    [通過第一個測試] - 新增 getDaysInDateRange()，處理 startDate 及 endDate 間有幾天，並存至 daysInDateRange 變數。 - 新增變數 daysInMonth，使用 moment 帶入 budgetDate 即可取得胎日期天數。 - 新增變數 budgetPerOneDay，計算 1 天需多少預算。 - 最後將 budgetPerOneDay 乘以 daysInDateRange 即可得到結果。

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -5,18 +5,29 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+import moment from 'moment';
 
-      }
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    budgetCalculator(budget, budgetDate, startDate, endDate) {
+      const daysInMonth = moment(budgetDate, 'YYYY-MM').daysInMonth();
+      const daysInDateRange = this.getDaysInDateRange(startDate, endDate);
+      const budgetPerOneDay = budget / daysInMonth;
+
+      return budgetPerOneDay * daysInDateRange;
     },
-    methods: {
-      budgetCalculator (budget, budgetDate, startDate, endDate) {
-        return 31;
-      }
-    }
-  }
+    getDaysInDateRange(startDate, endDate) {
+      const a = moment(startDate);
+      const b = moment(endDate);
+      return Math.abs(a.diff(b, 'days')) + 1;
+    },
+  },
+};
 </script>
 
 <style scoped>

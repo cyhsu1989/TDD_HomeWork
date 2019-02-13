fd14ce22ebe9522cb92ff437a3408266bcc58a65
Chin-Yu Hsu <cyhsu1989@gmail.com>
2019-02-13 09:34:04 +0800

    新增 Budget.vue 及 Budget.spec.js

---
+++ test/unit/specs/Budget.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils';
+import Budget from '@/components/Budget';
+
+describe('Budget Calculator', () => {
+  it('', () => {
+    // arrange
+    const wrapper = mount(Budget);
+
+    // act
+
+    // assert
+    wrapper.vm.doSomeWork();
+  });
+});
